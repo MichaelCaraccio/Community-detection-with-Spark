c4861aa18364dbd868e216cd91a6170df8c2673e
Michael Caraccio <michael.caraccio@gmail.com>
2015-04-28 14:49:31 +0200

    Code cleaned

--- scala/ConnectToCassandra/src/main/scala/ConnectToCassandra.scala
+++ scala/ConnectToCassandra/src/main/scala/ConnectToCassandra.scala
@@ -35,16 +35,6 @@ import scala.util.matching.Regex
 // https://bcomposes.wordpress.com/2013/02/09/using-twitter4j-with-scala-to-access-streaming-tweets/
 // https://github.com/datastax/spark-cassandra-connector/blob/master/doc/5_saving.md
 
-//put your keys and creds here 
-object Util {
-  val config = new twitter4j.conf.ConfigurationBuilder()
-    .setOAuthConsumerKey("MCrQfOAttGZnIIkrqZ4lQA9gr")
-    .setOAuthConsumerSecret("5NnYhhGdfyqOE4pIXXdYkploCybQMzFJiQejZssK4a3mNdkCoa")
-    .setOAuthAccessToken("237197078-6zwzHsuB3VY3psD5873hhU3KQ1lSVQlOXyBhDqpG")
-    .setOAuthAccessTokenSecret("UIMZ1aD06DObpKI741zC8wHZF8jkj1bh02Lqfl5cQ76Pl")
-    .build
-}
-
 object ConnectToCassandra {
     def main(args: Array[String]) {
 
@@ -54,62 +44,33 @@ object ConnectToCassandra {
 
         val filters = args
         
-        // Set the system properties so that Twitter4j library used by twitter stream
-        // can use them to generat OAuth credentials
-        /*System.setProperty("twitter4j.oauth.consumerKey", "MCrQfOAttGZnIIkrqZ4lQA9gr")
-        System.setProperty("twitter4j.oauth.consumerSecret", "5NnYhhGdfyqOE4pIXXdYkploCybQMzFJiQejZssK4a3mNdkCoa")
-        System.setProperty("twitter4j.oauth.accessToken", "237197078-6zwzHsuB3VY3psD5873hhU3KQ1lSVQlOXyBhDqpG")
-        System.setProperty("twitter4j.oauth.accessTokenSecret", "UIMZ1aD06DObpKI741zC8wHZF8jkj1bh02Lqfl5cQ76Pl")
-        System.setProperty("twitter4j.http.retryCount", "3");
-        System.setProperty("twitter4j.http.retryIntervalSecs", "10")
-        System.setProperty("twitter4j.async.numThreads", "1");
-        System.setProperty("twitter4j.http.useSSL", "false");*/
-        //System.setProperty("async.numThreads", "10");
-
+        // Spark configuration
         val sparkConf = new SparkConf(true)
         .setMaster("local[4]")
         .setAppName("ConnectToCassandra")
         .set("spark.cassandra.connection.host", "127.0.0.1") // Add this line to link to Cassandra
-
-
         
-        // Filters by words
+        // Filters by words that contains @
         val words = Array("@")
         
+        // Pattern used to find users
+        val pattern = new Regex("\\@\\w+")
         
-        //val austinBox = Array(Array(-97.8,30.25),Array(-97.65,30.35))
-        //val twitterStream = new TwitterStreamFactory(Util.config).getInstance
-        //twitterStream.filter(new FilterQuery().locations(austinBox))
-        
+        // First twitter instance : Used for stream
         val twitterstream = new TwitterFactory().getInstance()
         twitterstream.setOAuthConsumer("MCrQfOAttGZnIIkrqZ4lQA9gr", "5NnYhhGdfyqOE4pIXXdYkploCybQMzFJiQejZssK4a3mNdkCoa")
         twitterstream.setOAuthAccessToken(new AccessToken("237197078-6zwzHsuB3VY3psD5873hhU3KQ1lSVQlOXyBhDqpG", "UIMZ1aD06DObpKI741zC8wHZF8jkj1bh02Lqfl5cQ76Pl"))
+        System.setProperty("twitter4j.http.retryCount", "3");
+        System.setProperty("twitter4j.http.retryIntervalSecs", "10")
+        System.setProperty("twitter4j.async.numThreads", "1");
 
         val ssc = new StreamingContext(sparkConf, Seconds(1))
         val stream = TwitterUtils.createStream(ssc, Option(twitterstream.getAuthorization()), words)
         
-        
-        // Twitter Authentication credentials
-        /*val consumerKey = Play.current.configuration.getString("MCrQfOAttGZnIIkrqZ4lQA9gr").get
-        val consumerSecret = Play.current.configuration.getString("5NnYhhGdfyqOE4pIXXdYkploCybQMzFJiQejZssK4a3mNdkCoa").get
-        val accessToken = Play.current.configuration.getString("237197078-6zwzHsuB3VY3psD5873hhU3KQ1lSVQlOXyBhDqpG").get
-        val accessTokenSecret = Play.current.configuration.getString("UIMZ1aD06DObpKI741zC8wHZF8jkj1bh02Lqfl5cQ76Pl").get*/
-
-        // Authorising with your Twitter Application credentials
+        // Second twitter instance : Used to query user's informations
         val twitter = new TwitterFactory().getInstance()
         twitter.setOAuthConsumer("Vb0BxXrK933CDEeQ3Myj69kkC", "q55rXOM8pQnnAyPrYhHh6LHK4IFHw0U01tfe6VDoleaxmvOL3B")
         twitter.setOAuthAccessToken(new AccessToken("237197078-iXi3ANEAUXNmoDbcbH3lvS93vDO6PvEQj3255ToL", "Skv8J9xcfhbKV2Lwddke2g7llTDwwh6S9QyAlNR6fanqY"))
-        
-        
-        
-        
-        
-        
-
-        //val twcitter = new TwitterFactory().getInstance
-        //val statuses = twitter.search(new Query("#caca")).getTweets 
-        //val anus = twitter.showUser("2948436072")
-        val pattern = new Regex("\\@\\w+")
 
         // Stream about users
         val usersStream = stream.map{status => (status.getUser.getId.toString, 
@@ -123,8 +84,24 @@ object ConnectToCassandra {
         // Stream about tweets
         val tweetsStream = stream.map{status => (status.getId.toString, 
                                                  status.getUser.getId.toString, 
+                                                 status.getUser.getName.toString,
                                                  status.getText, 
-                                                 twitter.showUser(pattern.findFirstIn(status.getText).getOrElse("@MichaelCaraccio").tail),
+
+                                                    if(pattern.findFirstIn(status.getText).isEmpty){
+                                                        ""
+                                                    }
+                                                    else
+                                                    {
+                                                        twitterstream.showUser(pattern.findFirstIn(status.getText).getOrElse("@MichaelCaraccio").tail).getName
+                                                    },
+
+                                                    if(pattern.findFirstIn(status.getText).isEmpty){
+                                                        ""
+                                                    }
+                                                    else{
+                                                        twitterstream.showUser(pattern.findFirstIn(status.getText).getOrElse("@MichaelCaraccio").tail).getId
+                                                    },
+
                                                  status.getRetweetCount.toString,
                                                  new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(status.getCreatedAt),
 
@@ -142,8 +119,6 @@ object ConnectToCassandra {
                                                      ""
                                                  }
                                                 )}
-
-        //val twitter = TwitterFactory.getSingleton()
         
         // Save user's informations in Cassandra
         usersStream.foreachRDD(rdd => {
@@ -176,38 +151,21 @@ object ConnectToCassandra {
             
             
             rdd.foreach {r => {
-                // ****************************************		
-                // TEXT		
-                // ****************************************	
-                
-                // Pattern cherchant les @
-                //val pattern = new Regex("\\@\\w+")
-                
-                // Tweet
-                //val tweet_text_some = r._4	
+                val sender_name = r._3
                 val sender_id = r._2
-                val con = r._3
-                val wesh = r._4
-                
-                
-                //println("YOLO:" + (pattern findFirstIn con).tail)
-                
-                println("YOLO:" + pattern.findFirstIn(con).getOrElse("no match").tail)
-                println("TADAM:" + wesh)
-                
-                //statuses.foreach(status => println(status.getText + "\n"))
-                
-                //val test = twitter.getUser(sender_id)
-                
-                //println("DAT BITCH2" + reply)
+                val tweet_text = r._4
+                val dest_name = r._5               
+                val dest_id = r._6
 
-                
+                println("----------------------------------------------")
                 println("Sender ID : " + sender_id)
-                println("Sender Name : " + sender_id)
-                //println("DEst ID : " + tweet_text_some)
-                //println("Dest Name" + (pattern findAllIn tweet_text_some).mkString(","))
+                println("Sender Name : " + sender_name)
+                println("Tweet : " + tweet_text)
+                println("Dest name :" + dest_name)
+                println("Dest ID : " + dest_id)
+                println("----------------------------------------------")
+
             }}
-            //println(rdd->"tweet_text".value)
             println("tweet added")
         })
 
